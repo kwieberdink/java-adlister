424a96a47c5e84c040a0c2fc76050555b2a2828b
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-05-12 15:02:46 -0500

    error fix

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,13 +3,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        if(request.getSession().getAttribute("loggedIn" )!= null) {
+            if ((boolean) request.getSession().getAttribute("loggedIn")){
+                response.sendRedirect("/profile");
+                return;
+            }
+        } else {
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
     }
+
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
@@ -17,6 +26,7 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            request.getSession().setAttribute("loggedIn", true);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/webapp/ads/index.jsp
+++ src/main/webapp/ads/index.jsp
@@ -7,18 +7,10 @@
     </jsp:include>
 </head>
 <body>
-<jsp:include page="/partials/navbar.jsp" />
+    <jsp:include page="/partials/navbar.jsp" />
+    <div class="container">
 
-<div class="container">
-    <h1>Here Are all the ads!</h1>
-
-    <c:forEach var="ad" items="${ads}">
-        <div class="col-md-6">
-            <h2>${ad.title}</h2>
-            <p>${ad.description}</p>
-        </div>
-    </c:forEach>
-</div>
+    </div>
 
 </body>
 </html>
