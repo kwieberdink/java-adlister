9c13a1c662e79e1e96ca910c537939218b3027d0
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-05-12 20:31:05 -0500

    error fix

--- src/main/java/dao/JDBCTest.java
+++ src/main/java/dao/JDBCTest.java
@@ -2,15 +2,14 @@ package dao;
 
 import com.mysql.cj.jdbc.Driver;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JDBCTest {
 
     private static List<String> getAuthorNames() {
+
         List<String> authorNames = new ArrayList<>();
         try {
             // Searching for a Driver, and will find the mysql Driver located in pom.xml
@@ -18,12 +17,25 @@ public class JDBCTest {
             Connection connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/codeup_test_db?allowPublicKeyRetrieval=true&useSSL=false",
                     "root",
-                    "codeup"
-            );
+                    "codeup");
+            Statement statement = connection.createStatement();
+            // Execute statement
+            ResultSet resultSet = statement.executeQuery("SELECT name FROM codeup_test_db.authors");
+            while (resultSet.next()){
+                authorNames.add(resultSet.getString("name"));
+            }
         } catch (SQLException sqlx) {
             sqlx.printStackTrace();
         }
         return authorNames;
+    }
+
+    public static void main(String[] args) {
+
+        for(String name : getAuthorNames()) {
+            System.out.println(name);
+        }
+
     }
 
 }
