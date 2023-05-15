332c53f39501a7f78114e9e741de80c41762492f
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-05-15 14:04:22 -0500

    error fix

--- src/main/java/Dao/MySQLAdsDao.java
+++ src/main/java/Dao/MySQLAdsDao.java
@@ -2,9 +2,11 @@ package Dao;
 
 import Models.Ad;
 
+import java.sql.Connection;
 import java.util.List;
 
 public class MySQLAdsDao implements Ads {
+    private final Connection connection;
 
     @Override
     public List<Ad> all() {
