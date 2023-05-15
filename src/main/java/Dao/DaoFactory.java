9a8b0a04248cae7361bf4d92ec3eb24a8f4ff51a
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-05-15 16:37:58 -0500

    error fix

--- src/main/java/Dao/DaoFactory.java
+++ src/main/java/Dao/DaoFactory.java
@@ -1,12 +1,18 @@
 package Dao;
 
 public class DaoFactory {
+
     private static Ads adsDao;
+    private static Authors authorsDao;
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
             adsDao = new ListAdsDao();
         }
         return adsDao;
+    }
+
+    public static Authors getAuthorsDao() {
+
     }
 }
--- src/main/java/Dao/MySQLAdsDao.java
+++ src/main/java/Dao/MySQLAdsDao.java
@@ -1,19 +1,37 @@
 package Dao;
 
 import Models.Ad;
+import Config.Config.*;
+import com.mysql.cj.jdbc.Driver;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.List;
 
 public class MySQLAdsDao implements Ads {
     private final Connection connection;
 
-    public
-
-    @Override
-    public List<Ad> all() {
-        return null;
+    public MySQLAdsDao() {
+        try {
+            DriverManager.registerDriver(new Driver());
+            this.connection = DriverManager.getConnection(
+                    Config.Config.getUrl(),
+                    Config.Config.getUser(),
+                    Config.Config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+//
+//    @Override
+//    public List<Ad> all() {
+//        try {
+//            Statement statement = connection.createStatement();
+//            ResultSet resultSet = statement.executeQuery()
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     @Override
     public Long insert(Ad ad) {
